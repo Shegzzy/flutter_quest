4ba01ca1a108f2fafd923a3fb9aa1098b6eddc8a
Segun <miksegun1996@gmail.com>
2024-07-13 08:46:40 +0100

    added bloc for timer

---
+++ lib/src/bloc/timer_bloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_quest/src/bloc/state/timer_state.dart';
+import 'event/timer_event.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  TimerBloc() : super(TimerInitial());
+  StreamSubscription<int>? _tickerSubscription;
+
+  @override
+  Stream<TimerState> mapEventToState(TimerEvent event) async* {
+    if (event is StartTimer) {
+      _tickerSubscription?.cancel();
+      _tickerSubscription = _tick().listen((seconds) => add(Tick(seconds)));
+    } else if (event is Tick) {
+      yield TimerRunning(event.seconds);
+    }
+  }
+
+  Stream<int> _tick() async* {
+    int seconds = 0;
+    while (true) {
+      await Future.delayed(const Duration(seconds: 1));
+      yield seconds++;
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+}
